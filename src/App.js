a76868cc90d06e2153d9600c9d600304985b150f
Shehan Disanayake <dmcshehan@outlook.com>
2021-09-03 01:40:50 +0530

    finish featured component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+
+import Home from "./pages/home/home.jsx";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./app.css";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route to="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
